1bd3e766d1488e86f9f98d96fe2cd3058251eece
Jacob <spjak1@gmail.com>
2022-01-11 18:20:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # IpToInt
-VS Code Extension converting ip addresses to integers and back
+
+Convert IP addresses to integers and back
+
+## Features
+
+Converts an IPv4 address to a signed Int32 and vice versa.
+
+Use with `Ctrl+Shift+P` and search for `IpToInt`
+
+As default, mimics the behavior of .NET `System.BitConverter`, using Little Endian endianness.
+Settings allows modification of the endianness and sign in the conversion.
+
+Automatically copies the result to clipboard.
+This setting can be disabled with the `iptoint.copyToClipboard` setting.
+
+## Release Notes
+
+### 0.0.1
+
+Initial release
---
+++ extension.js
@@ -0,0 +1,74 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let ipToIntCommand = vscode.commands.registerCommand('iptoint.ipToInt', async function () {
+		let conf = vscode.workspace.getConfiguration()
+		let ip = await vscode.window.showInputBox({prompt: "IP address"})
+		let converted = ipToInt(ip)
+		vscode.window.showInformationMessage(`${converted}`)
+		if (conf.get("iptoint.copyToClipboard")) {
+			vscode.env.clipboard.writeText(`${converted}`)
+		}
+	});
+
+	context.subscriptions.push(ipToIntCommand);
+
+	let intToIpCommand = vscode.commands.registerCommand('iptoint.intToIp', async function () {
+		let conf = vscode.workspace.getConfiguration()
+		let integer = await vscode.window.showInputBox({prompt: "Integer"})
+		let converted = intToIp(integer)
+		vscode.window.showInformationMessage(`${converted}`)
+		if (conf.get("iptoint.copyToClipboard")) {
+			vscode.env.clipboard.writeText(`${converted}`)	
+		}
+	});
+
+	context.subscriptions.push(intToIpCommand);
+}
+
+function deactivate() {}
+
+function ipToInt(ip) {
+    var parts = ip.split(".")
+	const buf = Buffer.from([parseInt(parts[0], 10), parseInt(parts[1], 10), parseInt(parts[2], 10), parseInt(parts[3], 10)])
+	if (vscode.workspace.getConfiguration().get("iptoint.endian") == "bigendian") {
+		if (vscode.workspace.getConfiguration().get("iptoint.sign") == "unsigned") {
+			return buf.readUInt32BE(0)
+		}
+		return buf.readInt32BE(0)
+	}
+	if (vscode.workspace.getConfiguration().get("iptoint.sign") == "unsigned") {
+		return buf.readUInt32LE(0)
+	}
+	return buf.readInt32LE(0)
+}
+
+function intToIp(integer) {
+	const buf = Buffer.allocUnsafe(4)
+	if (vscode.workspace.getConfiguration().get("iptoint.endian") == "bigendian") {
+		if (vscode.workspace.getConfiguration().get("iptoint.sign") == "unsigned") {
+			buf.writeUInt32BE(integer, 0)
+		}
+		else {
+			buf.writeInt32BE(integer, 0)
+		}
+	}
+	else {
+		if (vscode.workspace.getConfiguration().get("iptoint.sign") == "unsigned") {
+			buf.writeUInt32LE(integer, 0)
+		}
+		else {
+			buf.writeInt32LE(integer, 0)
+		}
+	}
+	return buf.join(".")
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
